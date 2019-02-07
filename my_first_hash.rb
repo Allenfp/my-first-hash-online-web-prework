ec02170caf97dd94ca76e378e3642f476780a2d1
Allen F Partlow <allenfp@gmail.com>
2019-02-07 20:44:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,8 +12,9 @@ def shipping_manifest()
 
   the_manifest = Hash.new
   the_manifest['whale bone corsets'] = 5
-  the_manifest['porcelain vases'] = 2
   the_manifest['oil paintings'] = 3
+  the_manifest['porcelain vases'] = 2
+
 
   the_manifest
 end
